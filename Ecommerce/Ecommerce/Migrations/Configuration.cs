1680f29f4f54f5f30435987074bfe27bee9d603d
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2019-03-06 01:37:33 -0500

    Departments and Citites basic CRUD

---
+++ Ecommerce/Ecommerce/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace Ecommerce.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Ecommerce.Models.ECommerceContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "Ecommerce.Models.ECommerceContext";
+        }
+
+        protected override void Seed(Ecommerce.Models.ECommerceContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Ecommerce/Ecommerce/Models/ECommerceContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    public class ECommerceContext : DbContext
+    {
+        public ECommerceContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Ecommerce.Models.Department> Departments { get; set; }
+
+        public System.Data.Entity.DbSet<Ecommerce.Models.City> Cities { get; set; }
+    }
+}
